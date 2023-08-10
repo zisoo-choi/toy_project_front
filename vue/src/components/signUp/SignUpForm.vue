8ccdf69cd919eed81c6a2c33eca7e5653708a308
zisoo <zisoo_3@naver.com>
2023-08-10 17:47:48 +0900

    style: [최지수] 회원 가입 디자인 수정

--- vue/src/components/signUp/SignUpForm.vue
+++ vue/src/components/signUp/SignUpForm.vue
@@ -1,9 +1,9 @@
 <template>
     <form @submit.prevent="onSubmit" class="signup-form">
-        <input type="text" v-model="userId" placeHolder="아이디"/>
-        <input type="password" v-model="userPw" placeHolder="비밀번호"/>
-        <input type="text" v-model="userAddress" placeHolder="주소"/>
-        <input type="text" v-model="userPhone" placeHolder="전화번호"/>
+        <input class="formLine" type="text" v-model="userId" placeHolder="아이디"/>
+        <input class="formLine" type="password" v-model="userPw" placeHolder="비밀번호"/>
+        <input class="formLine" type="text" v-model="userAddress" placeHolder="주소"/>
+        <input class="formLine" type="text" v-model="userPhone" placeHolder="전화번호"/>
         <v-btn @click="onSubmit" color="primary">가입하기</v-btn>
     </form>
 </template>
@@ -32,14 +32,27 @@ export default {
 </script>
 
 <style lang="css">
-    .signup-form {
+.signup-form {
     display: flex;
     flex-direction: column; /* 세로로 나열되도록 설정 */
     align-items: center; /* 가운데 정렬 */
 }
-
 .input-field {
     margin: 10px 0; /* 각 input 사이의 간격 조정 */
     padding: 5px;
+}
+.formLine{
+    width: 300px;
+    height: 50px;
+    background-color: rgb(233, 233, 233);
+    border-radius: 5px;
+    margin: 5px;
+}
+.formLine:hover{
+    width: 300px;
+    height: 50px;
+    background-color: rgb(247, 247, 247);
+    border-radius: 5px;
+    margin: 5px;
 }
 </style>
--- vue/src/store/signUp/actions.js
+++ vue/src/store/signUp/actions.js
@@ -1,18 +1,19 @@
 import axiosInst from "@/utility/axiosInst";
 
 export default {
-  requestSignUpToSpring(context, payload) {
+  requestSignUpToSpring(_, payload) {
     const { userId, userPw, userAddress, userPhone } = payload;
 
     console.log("userId: ", userId,", userPw:", userPw)
     console.log("userAddress: ", userAddress,", userPhone:", userPhone)
     
     return axiosInst.post("/member/sign-up", { userId, userPw, userAddress, userPhone })
-        .then(() => {
+        .then((res) => {
           alert("회원가입이 완료되었습니다.");
+          console.log(res);
         })
         .catch(() => {
-          alert("문제 발생!");
+          console.error
         });
   }
 }
